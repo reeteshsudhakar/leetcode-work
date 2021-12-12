fa228b87c141e33e3f1029f0ef6d741e2970dec1
Reetesh Sudhakar <reesud6187@gmail.com>
2021-12-11 21:29:56 -0500

    Time: 12 ms (97.75%), Space: 13.6 MB (13.40%) - LeetHub

---
+++ reverse-integer/reverse-integer.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        min = -2**31
+        max = 2**31 - 1
+        
+        if x == 0:
+            return 0
+        
+        elif x > 0: 
+            reverse = int(str(x)[::-1])
+            if reverse > min and reverse < max:
+                return reverse
+            else: 
+                return 0
+        
+        elif x < 0:
+            reverse = int(str(abs(x))[::-1]) * -1
+            if reverse > min and reverse < max:
+                return reverse
+            else: 
+                return 0
