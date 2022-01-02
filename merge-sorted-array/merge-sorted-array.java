650fd24d14c69de2a7c79263c304736ffe3e0bd6
Reetesh Sudhakar <reesud6187@gmail.com>
2022-01-01 23:19:56 -0800

    Time: 0 ms (100.00%), Space: 39.7 MB (8.82%) - LeetHub

--- merge-sorted-array/merge-sorted-array.java
+++ merge-sorted-array/merge-sorted-array.java
@@ -1,10 +1,13 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         for (int i = m + n - 1; i >= 0; i--) {
+            
+            // checks if the second array is empty
             if (n == 0) {
                 return;
             }
             
+            // if the number in nums1 is greater than the number in nums2, the number in nums1 gets moved appropriately.
             if (m > 0 && nums1[m - 1] > nums2[n - 1]) {
                 nums1[i] = nums1[m - 1];
                 m--;
