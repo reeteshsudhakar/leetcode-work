cdbb9d50164f222f9535f0f7f257fa17d340b0de
Reetesh Sudhakar <reesud6187@gmail.com>
2022-03-24 22:19:17 -0700

    Time: 1 ms (99.00%), Space: 55.2 MB (52.20%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
@@ -11,28 +11,35 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        
+        // if the head is null, return null 
         if (headA == null || headB == null) {
             return null;
         }
         
+        // initializing variables 
         int lengthA = 0; 
         ListNode nodeA = headA; 
         int lengthB = 0; 
         ListNode nodeB = headB; 
         
+        // calculating length of list A 
         while (nodeA != null) {
             lengthA++;
             nodeA = nodeA.next;
         }
         
+        // calculating length of list B 
         while (nodeB != null) {
             lengthB++;
             nodeB = nodeB.next; 
         }
         
+        // resetting nodes to skip to the right place in each list
         nodeA = headA;
         nodeB = headB; 
         
+        // if A is greater in length, skips to node which makes length equal 
         if (lengthA > lengthB) {
             int diff = lengthA - lengthB; 
             while (diff > 0) {
@@ -40,6 +47,7 @@ public class Solution {
                 diff--; 
             }
         } else {
+            // if B is greater in length, skips to node which makes length equal 
             int diff = lengthB - lengthA; 
             while (diff > 0) {
                 nodeB = nodeB.next;
@@ -47,11 +55,13 @@ public class Solution {
             }
         }
         
+        // while they're not null and not equal, keep moving through the list 
         while (nodeA != null && nodeB != null && nodeA != nodeB) {
             nodeA = nodeA.next;
             nodeB = nodeB.next; 
         }
         
+        // if they're equal at a node, the while loop breaks and returns node A (can return either)
         return nodeA; 
     }
 }
