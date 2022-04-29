5c297904f1a90e32784f9e57425d38e1867f995b
Reetesh Sudhakar <reesud6187@gmail.com>
2022-04-29 18:36:17 -0400

    Time: 1761 ms (7.00%), Space: 14.8 MB (45.63%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        result = ""
+        for char in s: 
+            if char.isalnum() == True: 
+                result += char.lower()
+        return result == result[::-1]
