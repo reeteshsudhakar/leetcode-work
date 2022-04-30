0dc028a44fadeff790dee501650bfb3950f199b2
Reetesh Sudhakar <reesud6187@gmail.com>
2022-04-29 22:54:50 -0400

    Time: 0 ms (100.00%), Space: 40.5 MB (64.99%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int climbStairs(int n) {
+        if (n <= 3) {
+            return n;
+        } else {
+            int[] arr = new int[n + 1];
+            
+            arr[1] = 1;
+            arr[2] = 2;
+            
+            for (int i = 3; i < arr.length; i++) {
+                arr[i] = arr[i - 1] + arr[i - 2];
+            }
+            
+            return arr[n];
+        }
+    }
+}
