3ebfc2144b5655efdb953475f95611a0e06a4fca
Reetesh Sudhakar <reesud6187@gmail.com>
2022-05-01 21:18:32 -0400

    Time: 4 ms (94.43%), Space: 41.8 MB (98.48%) - LeetHub

---
+++ 205-isomorphic-strings/205-isomorphic-strings.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        for (int i = 0; i < s.length(); i++) {
+            if (s.lastIndexOf(s.charAt(i)) != t.lastIndexOf(t.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
