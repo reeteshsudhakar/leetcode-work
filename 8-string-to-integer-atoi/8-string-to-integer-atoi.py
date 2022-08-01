d93f53dbbbe2b9ed48fd1cfd8f5145b647527b9e
Reetesh Sudhakar <reesud6187@gmail.com>
2022-08-01 12:48:12 -0700

    Time: 49 ms (19.48%), Space: 13.7 MB (23.53%) - LeetHub

--- 8-string-to-integer-atoi/8-string-to-integer-atoi.py
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.py
@@ -5,6 +5,7 @@ class Solution(object):
         :rtype: int
         """
         
+        # initializing variables
         idx = 0
         length = len(s)
         sign = 1
@@ -18,16 +19,19 @@ class Solution(object):
         if idx < length and s[idx] == "-":
             sign = -1
             idx += 1
+        # passing the plus sign if found, not needed 
         elif idx < length and s[idx] == "+":
             idx += 1
         
-        # parsing the digits
+        # parsing the digits and tallying the sum 
         while idx < length and s[idx].isdigit():
             result = result * 10 + int(s[idx])
             idx += 1
         
+        # accounting for the sign in the string
         result *= sign 
         
+        # returning the result based on constraints
         if (result > 2**31 - 1):
             return 2**31 - 1
         elif (result < -2**31):
