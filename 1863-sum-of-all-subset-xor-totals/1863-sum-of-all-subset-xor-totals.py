a17f68557798bc789d69b9cb26aec24b09fac325
Reetesh Sudhakar <reesud6187@gmail.com>
2024-05-20 15:56:35 -0700

    Time: 43 ms (74.29%), Space: 16.5 MB (48.32%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.py
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.py
@@ -3,10 +3,9 @@ class Solution:
         def dfs(idx: int, curr_xor: int) -> int:
             if idx == len(nums): return curr_xor
             
-            exclude = dfs(idx + 1, curr_xor)
             include = dfs(idx + 1, curr_xor ^ nums[idx])
+            exclude = dfs(idx + 1, curr_xor)
             
             return include + exclude
         
         return dfs(0, 0)
-            
