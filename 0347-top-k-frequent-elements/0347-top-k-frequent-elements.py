1221d0bcddb09bce73e57da34de053aa35515dd5
Reetesh Sudhakar <reesud6187@gmail.com>
2024-05-19 21:50:20 -0700

    Time: 90 ms (51.19%), Space: 21.4 MB (34.50%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,17 +1,12 @@
 from collections import defaultdict
 
-class Solution(object):
-    def topKFrequent(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: List[int]
-        """
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        ret = defaultdict(int)
         
-        d = defaultdict(int)
+        for num in nums:
+            ret[num] += 1
         
-        for i in nums: 
-            d[i] += 1
+        ret_list = sorted(ret.items(), key=lambda x: x[1], reverse=True)
         
-        return sorted(d, key = d.get, reverse = True)[:k]
-                
+        return [item[0] for item in ret_list][:k]
