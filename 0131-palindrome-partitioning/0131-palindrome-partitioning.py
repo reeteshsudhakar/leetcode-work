4579103b54a0929219de53967439aaf75ab8cff0
Reetesh Sudhakar <reesud6187@gmail.com>
2024-05-22 17:47:06 -0700

    Time: 479 ms (31.43%), Space: 35.1 MB (76.81%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,32 +1,5 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-#         ret = []
-        
-#         def check_palindrome(arr: List[str]) -> bool:
-#             for item in arr: 
-#                 if item != item[::-1]: 
-#                     return False
-#             return True
-        
-#         def dfs(idx: int, curr_arr: List[str]):
-#             if idx == len(s):
-#                 if check_palindrome(curr_arr) and curr_arr not in ret: 
-#                     ret.append(curr_arr)
-#                 return
-            
-#             dfs(idx + 1, curr_arr + [s[idx]])
-            
-#             diff_arr = curr_arr.copy()
-#             if len(diff_arr) > 0: 
-#                 diff_arr[-1] += s[idx]
-#             else: 
-#                 diff_arr += s[idx]
-            
-#             dfs(idx + 1, diff_arr)
-        
-#         dfs(0, [])
-#         return ret
-
         ret = []
         
         def check_palindrome(val: str) -> bool:
