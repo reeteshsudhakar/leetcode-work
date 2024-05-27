62b5623e9448ca6e194248c27adaa58a216b514b
Reetesh Sudhakar <reesud6187@gmail.com>
2024-05-26 23:46:53 -0700

    Time: 14 ms (64.84%), Space: 11.8 MB (25.45%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -5,18 +5,13 @@ class Solution(object):
         :rtype: bool
         """
         stack = []
-        start_chars = ['[', '(', '{']
-
+        starts = ['[', '(', '{']
+        
         for char in s:
-            if char in start_chars:
-                stack.append(char)
-            elif stack and char == ']' and stack[-1] == '[':
-                stack.pop()
-            elif stack and char == ')' and stack[-1] == '(':
-                stack.pop()
-            elif stack and char == '}' and stack[-1] == '{':
-                stack.pop()
-            else:
-                stack.append(char)
+            if char in starts: stack.append(char)
+            elif stack and char == ']' and stack[-1] == '[': stack.pop()
+            elif stack and char == ')' and stack[-1] == '(': stack.pop()
+            elif stack and char == '}' and stack[-1] == '{': stack.pop()
+            else: stack.append(char)
         
         return len(stack) == 0
